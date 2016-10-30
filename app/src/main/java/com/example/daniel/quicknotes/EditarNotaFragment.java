fcf88087a85d2d510ca7c7f7b4c5675455e3336a
Daniel <fdsc.lbck@gmail.com>
2016-10-29 23:13:56 -0500

    Se esta corrigiendo un error con la funcionalidad de agregar

--- app/src/main/java/com/example/daniel/quicknotes/EditarNotaFragment.java
+++ app/src/main/java/com/example/daniel/quicknotes/EditarNotaFragment.java
@@ -17,10 +17,14 @@ import android.widget.EditText;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class EditarNotaFragment extends Fragment {
+public class EditarNotaFragment extends Fragment{
 
-
+    private EditText titulo;
+    private EditText descripcion;
+    private int tipo = 1;
     private AlertDialog popupGuardar;
+
+    private boolean newNote = false;
 
     public EditarNotaFragment() {
         // Required empty public constructor
@@ -31,16 +35,21 @@ public class EditarNotaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        Bundle bundle = this.getArguments();
+        if(bundle != null){
+            newNote = bundle.getBoolean(notaDetalleActivity.NEW_NOTE_EXTRA, false);
+        }
+
         View fragmentLayout = inflater.inflate(R.layout.fragment_editar_nota, container, false);
-        EditText titulo = (EditText) fragmentLayout.findViewById(R.id.editarTituloNota);
-        EditText descripcion = (EditText) fragmentLayout.findViewById(R.id.editarDescripcionNota);
-        Button botonGuardar = (Button) fragmentLayout.findViewById(R.id.guardarCambio);
+        titulo = (EditText) fragmentLayout.findViewById(R.id.editarTituloNota);
+        descripcion = (EditText) fragmentLayout.findViewById(R.id.editarDescripcionNota);
+        Button botonGuardar = (Button)fragmentLayout.findViewById(R.id.guardarCambio) ;
 
 
         popupConfirm();
-        botonGuardar.setOnClickListener(new View.OnClickListener() {
+        botonGuardar.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 popupGuardar.show();
             }
 
@@ -49,26 +58,36 @@ public class EditarNotaFragment extends Fragment {
 
         Intent intent = getActivity().getIntent();
         titulo.setText(intent.getExtras().getString(pantallaPrincipal.NOTA_TITLE_EXTRA, ""));
-        descripcion.setText(intent.getExtras().getString(pantallaPrincipal.NOTA_DESCRIPCION_EXTRA, ""));
+        descripcion.setText(intent.getExtras().getString(pantallaPrincipal.NOTA_DESCRIPCION_EXTRA,""));
         return fragmentLayout;
     }
 
-    private void popupConfirm() {
+    private void popupConfirm(){
         AlertDialog.Builder confirm = new AlertDialog.Builder(getActivity());
         confirm.setTitle("Confirmacion");
         confirm.setMessage("¿Desea guardar esta nota?");
 
-        confirm.setPositiveButton("Guardar", new DialogInterface.OnClickListener() {
+        confirm.setPositiveButton("Guardar", new DialogInterface.OnClickListener(){
             @Override
-            public void onClick(DialogInterface dialogo, int opcion) {
+            public void onClick(DialogInterface dialogo , int opcion){
+
+                NotesDBHelper dbAdapter = new NotesDBHelper(getActivity().getBaseContext());
+                dbAdapter.open();
+                if(newNote){
+                    dbAdapter.crearNota(titulo.getText() + "", descripcion.getText()+ "", tipo );
+                }
+                else
+                {
+
+                }
                 Intent intent = new Intent(getActivity(), pantallaPrincipal.class);
                 startActivity(intent);
 
             }
         });
-        confirm.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+        confirm.setNegativeButton("Cancelar", new DialogInterface.OnClickListener(){
             @Override
-            public void onClick(DialogInterface dialogo, int opcion) {
+            public void onClick(DialogInterface dialogo, int opcion){
 
             }
         });
@@ -77,4 +96,5 @@ public class EditarNotaFragment extends Fragment {
 
 
     }
+
 }
